ae78083479697adfbe6b304867261c46ee7524ef
Pratim Kanrar <kanrar.pratim@gmail.com>
2025-03-26 18:05:23 +0530

    ui update

--- code/src/api/app.py
+++ code/src/api/app.py
@@ -33,10 +33,6 @@ ai_model = GenerativeAIModel()
 
 prepareChatHistory()
 
-# Directory to save uploaded .eml files
-UPLOAD_FOLDER = "uploaded_emails"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-
 @app.route('/generate', methods=['POST'])
 def generate_response():
     try:
@@ -48,14 +44,9 @@ def generate_response():
         # Get the .eml file content from the request
         eml_content = input_data["message"]
 
-        # Save the .eml content to a new file
-        eml_file_path = os.path.join(UPLOAD_FOLDER, input_data["fileName"])
-        with open(eml_file_path, "w", encoding="utf-8") as eml_file:
-            eml_file.write(eml_content)
-
         # Pass the .eml content to the Generative AI model
         response_text = ai_model.generate_response(eml_content)
-        
+
         # Return the response
         return jsonify({"response": response_text}), 200
     except Exception as e:
